2ae8581773cc5cc19b211bea1bd0d9026bb7c6db
lde2325 <lde2325@naver.com>
2023-11-03 21:45:36 +0900

    admin

--- src/App.js
+++ src/App.js
@@ -1,27 +1,17 @@
 import React, { useState } from "react";
 import Home from "./components/Home";
 import Container from "./components/Container";
-import XXX from "./components/XXX";
 
 const App = () => {
-  const [currentComponent, setCurrentComponent] = useState("HOME");
-  const workers = [
-    { name: "A", checked: true },
-    { name: "B", checked: false },
-  ];
+  const [currentComponent, setCurrentComponent] = useState(null);
+  const [clicked, setClicked] = useState(null);
+
+  const workers = [{ name: "A" }, { name: "B" }];
   let displayedComponent;
-  switch (currentComponent) {
-    case "CONTAINER":
-      displayedComponent = <Container />;
-      break;
-    case "XXX":
-      displayedComponent = <XXX />;
-      break;
-    case "VALIDATION":
-      displayedComponent = <XXX />;
-      break;
-    default:
-      displayedComponent = <Home />;
+  if (currentComponent) {
+    displayedComponent = <Container clickedindex={clicked} />;
+  } else {
+    displayedComponent = <Home />;
   }
 
   const renderMenuItem = (componentName, label) => {
@@ -39,14 +29,17 @@ const App = () => {
           {currentComponent === componentName ? (
             <div style={{ marginLeft: "20px" }}>
               {workers.map((worker, index) => (
-                <div style={{ display: "flex", alignItems: "center" }}>
+                <div
+                  style={{ display: "flex", alignItems: "center" }}
+                  onClick={() => {
+                    setClicked(index + 1);
+                  }}
+                >
                   <div
                     key={index}
-                    onClick={() => {}}
                     style={{
                       width: "10px",
                       height: "10px",
-                      backgroundColor: worker.checked ? "black" : "white",
                       border: "1px solid black",
                       marginRight: "10px",
                     }}
@@ -78,7 +71,7 @@ const App = () => {
       >
         <ul style={{ listStyleType: "none", padding: 0, marginTop: "20%" }}>
           {renderMenuItem("CONTAINER", "컨테이너 터미널")}
-          {renderMenuItem("XXX", "관세 및 통관")}
+          {renderMenuItem("TAX", "관세 및 통관")}
           {renderMenuItem("VALIDATION", "화물 검사 지점")}
         </ul>
       </div>
--- src/components/Container.js
+++ src/components/Container.js
@@ -1,19 +1,124 @@
 import React from "react";
+import { useState } from "react";
+import { useEffect } from "react";
 
-const Container = () => {
-  return (
-    <div style={{ flex: 1 }}>
-      <h1>울산항만공사</h1>
-      <div
-        style={{
-          width: "100%",
-          height: "5px",
-          backgroundColor: "black",
-        }}
-      />
-      <div>CONTAINER</div>
-    </div>
-  );
+const Container = ({ clickedindex }) => {
+  const [clicked, setClicked] = useState(clickedindex);
+  const [boldIndex, setBoldIndex] = useState(null);
+
+  useEffect(() => {
+    setClicked(clickedindex);
+  }, [clickedindex]);
+
+  const demoData = [
+    {
+      title: "카드1",
+      content: ["항목1 설명...", "항목2 설명...", "항목3 설명..."],
+    },
+    {
+      title: "카드2",
+      content: ["항목1 설명...", "항목2 설명...", "항목3 설명..."],
+    },
+    {
+      title: "카드3",
+      content: ["항목1 설명...", "항목2 설명...", "항목3 설명..."],
+    },
+  ];
+  const first = () => {
+    return (
+      <div style={{ flex: 1 }}>
+        <h1>울산항만공사</h1>
+        <div
+          style={{
+            width: "100%",
+            height: "6px",
+            backgroundColor: "black",
+          }}
+        />
+        <div style={{ display: "flex" }}>
+          {demoData.map((card, index) => (
+            <div
+              key={index}
+              style={{
+                border: "1px solid black",
+                padding: "20px",
+                margin: "10px",
+                width: "200px",
+              }}
+              onClick={() => {
+                setClicked(index + 1);
+              }}
+            >
+              <h3>{card.title}</h3>
+              <ul>
+                {card.content.map((item, idx) => (
+                  <li key={idx}>{item}</li>
+                ))}
+              </ul>
+            </div>
+          ))}
+        </div>
+      </div>
+    );
+  };
+
+  const customer = () => {
+    return (
+      <div style={{ flex: 1 }}>
+        <h1 onClick={() => setClicked(null)}>
+          컨테이너 터미널 _ {demoData[clicked - 1].title}
+        </h1>
+        <div
+          style={{
+            width: "100%",
+            height: "6px",
+            backgroundColor: "black",
+            marginBottom: "10px",
+          }}
+        />
+        <div
+          style={{
+            display: "flex",
+            width: "100%",
+            height: "80%",
+            justifyContent: "space-between",
+          }}
+        >
+          <div>
+            {demoData[clicked - 1].content.map((item, index) => (
+              <div
+                key={index}
+                style={{
+                  marginLeft: "10px",
+                  marginBottom: "10px",
+                  display: "flex",
+                  alignItems: "center",
+                  fontWeight: boldIndex === index ? "bold" : "normal",
+                }}
+                onClick={() => setBoldIndex(index)}
+              >
+                <div
+                  style={{
+                    width: "10px",
+                    height: "10px",
+                    border: "solid",
+                    marginRight: "10px",
+                  }}
+                />
+                {item}
+              </div>
+            ))}
+          </div>
+
+          <div
+            style={{ backgroundColor: "grey", width: "40%", height: "100%" }}
+          ></div>
+        </div>
+      </div>
+    );
+  };
+
+  return clicked ? customer() : first();
 };
 
 export default Container;
